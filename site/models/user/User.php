b30b37c891c417a94ea769a4d1a0732060339782
flareair <flareair0@gmail.com>
2015-10-26 18:39:09 +0300

    Added user model class bootstrap

---
+++ site/models/user/User.php
@@ -0,0 +1,64 @@
+<?php
+
+use yii\db\ActiveRecord;
+use yii\web\IdentityInterface;
+use Yii;
+
+class User extends ActiveRecord implements IdentityInterface
+{
+    public static function tableName()
+    {
+        return 'user';
+    }
+
+    public function rules()
+    {
+        return [
+            [['login', 'password', 'auth_key'], 'string', 'max' => 255],
+            [['login'], 'unique']
+        ];
+    }
+
+    public function beforeSave($insert)
+    {
+        if (parent::beforeSave($insert)) {
+            if ($this->isNewRecord) {
+                $this->auth_key = Yii::$app->security->generateRandomString();
+            }
+            if ($this->isAttributeChanged('password')) {
+                $this->password = Yii::$app->security->generatePasswordHash(
+                    $this->password
+                );
+            }
+
+            return true;
+        }
+        return false;
+    }
+
+
+    public static function findIdentity($id)
+    {
+        return static::findOne($id);
+    }
+
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        return static::findOne(['access_token' => $token]);
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getAuthKey()
+    {
+        return $this->auth_key;
+    }
+
+    public function validateAuthKey($authKey)
+    {
+        return $this->getAuthKey() === $authKey;
+    }
+}
