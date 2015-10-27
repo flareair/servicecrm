9dfbb4d9c873425c537e45ca1dc265af228c3cf1
flareair <flareair0@gmail.com>
2015-10-27 12:26:14 +0300

    Added test users to migration table

--- site/migrations/m151026_135904_add_user_table.php
+++ site/migrations/m151026_135904_add_user_table.php
@@ -2,20 +2,42 @@
 
 use yii\db\Schema;
 use yii\db\Migration;
+use app\models\user\User;
 
 class m151026_135904_add_user_table extends Migration
 {
+    private $users = [
+        [
+            'username' => 'admin',
+            'password' => '1234',
+        ],
+        [
+            'username' => 'serviceman',
+            'password' => '1234',
+        ],
+        [
+            'username' => 'user',
+            'password' => '1234',
+        ]
+    ];
+
     public function up()
     {
         $this->createTable('user', [
                 'id' => 'pk',
-                'login' => 'string unique',
+                'username' => 'string unique',
                 'password' => 'string',
                 'auth_key' => 'string unique'
 
             ],
             'ENGINE=InnoDB'
         );
+
+        foreach ($this->users as $userData) {
+            $user = new User();
+            $user->attributes = $userData;
+            $user->save();
+        }
     }
 
     public function down()
