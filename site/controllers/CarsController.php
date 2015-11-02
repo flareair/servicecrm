ca6b80f06b109759945e9c84f85f16cf96bd48ed
flareair <flareair0@gmail.com>
2015-11-02 15:55:06 +0300

    Fixed problem with cars update action

--- site/controllers/CarsController.php
+++ site/controllers/CarsController.php
@@ -45,14 +45,14 @@ class CarsController extends Controller
 
     public function actionUpdate($id)
     {
-        $serviceMans = User::getServiceMans();
+        $users = User::getClients();
         $model = $this->findCar($id);
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
             return $this->redirect(['view', 'id' => $model->id]);
         } else {
             return $this->render('update', [
                 'model' => $model,
-                'serviceMans' => $serviceMans
+                'users' => $users
             ]);
         }
     }
